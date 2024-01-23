19c1cc02268b910a8ae087a507f7f2889cfd48d2
james-door <103546187+james-door@users.noreply.github.com>
2024-01-23 13:53:34 +1000

    Make Win32 Executable

--- Window.cpp
+++ Window.cpp
@@ -1,12 +1,54 @@
 ﻿// Window.cpp : Defines the entry point for the application.
 //
 
-#include "Window.h"
+#include <iostream>
+#include <sstream>
 
-using namespace std;
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 
-int main()
+std::stringstream Win32ErrorLog;
+
+inline void LogIfFailed(bool err, const char* file, int line)
 {
-	cout << "Hello CMake." << endl;
+    if (err)
+    {
+        DWORD error = GetLastError();
+        LPVOID lpMsgBuf;
+        FormatMessageA(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER |
+            FORMAT_MESSAGE_FROM_SYSTEM |
+            FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL,
+            error,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            (LPSTR)&lpMsgBuf,
+            0, NULL);
+
+
+        std::string message = "File: " + std::string(file) + "\n\nMessage: " + std::string((char*)lpMsgBuf) + "\n\nLine: " + std::to_string(line);
+        LocalFree(lpMsgBuf);
+
+        Win32ErrorLog << message;
+    }
+}
+void displayErrorMessage(std::string error) {
+
+    MessageBoxA(NULL, error.c_str(), NULL, MB_OK);
+
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
+{
+
+
+
+
+
+
+    if (!Win32ErrorLog.str().empty()) {
+        displayErrorMessage("Win32 Errors:\n\n" + Win32ErrorLog.str());
+    }
+
 	return 0;
 }
