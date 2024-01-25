8c5315f41be8ee56797ce08a4c7a9b48af459486
james-door <103546187+james-door@users.noreply.github.com>
2024-01-25 16:00:02 +1000

    Create the Window

--- Window.cpp
+++ Window.cpp
@@ -1,12 +1,14 @@
-﻿// Window.cpp : Defines the entry point for the application.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <sstream>
-
 #define WIN32_LEAN_AND_MEAN
+
 #include <Windows.h>
 
+#define HANDLE_RETURN(err) LogIfFailed(err, __FILE__, __LINE__)
+
+
+constexpr LONG defaultWindowWidth = 990;
+constexpr LONG defaultWindowHeight = 540;
 std::stringstream Win32ErrorLog;
 
 inline void LogIfFailed(bool err, const char* file, int line)
@@ -38,8 +40,73 @@ void displayErrorMessage(std::string error) {
 
 }
 
+//Message Procedure
+LRESULT WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+    return 0;
+
+}
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
+    //Register the Window
+    WNDCLASSEX winClass = {};
+    winClass.cbSize = sizeof(WNDCLASSEX);
+    winClass.style = CS_HREDRAW | CS_VREDRAW;
+    winClass.lpfnWndProc = WndProc;
+    winClass.cbClsExtra = 0;
+    winClass.cbWndExtra = 0;
+    winClass.hInstance = hInstance;
+    winClass.hIcon = NULL;
+    winClass.hCursor = LoadCursor(hInstance, NULL);
+    winClass.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    winClass.lpszMenuName = "window menu";
+    winClass.lpszClassName = "window";
+    winClass.hIconSm = LoadIcon(hInstance, NULL);
+
+    HANDLE_RETURN(RegisterClassEx(&winClass) == 0);
+
+
+    //Fill windowArea RECT with Client area
+    RECT windowArea = {};
+    windowArea.left = 0;
+    windowArea.top = 0;
+    windowArea.right = defaultWindowWidth;
+    windowArea.bottom = defaultWindowHeight;
+
+    //Find window area from the client 
+    HANDLE_RETURN(AdjustWindowRect(&windowArea, WS_OVERLAPPEDWINDOW, TRUE) == 0);
+    int windowAreaWidth = static_cast<int>(windowArea.right - windowArea.left);
+    int windowAreaHeight = static_cast<int>(windowArea.bottom - windowArea.top);
+    //Find centre of screen coordinates
+    int displayWidth = GetSystemMetrics(SM_CXSCREEN);
+    int displayHeight = GetSystemMetrics(SM_CYSCREEN);
+    int centreX = (displayWidth - windowAreaWidth) / 2;
+    int centreY = (displayHeight - windowAreaHeight) / 2;
+
+    //Create the window
+    HWND windowHandle = CreateWindowEx(NULL, "window", "My Window", WS_OVERLAPPEDWINDOW, centreX, centreY,
+        windowAreaWidth, windowAreaHeight, NULL, NULL, hInstance, nullptr);
+    HANDLE_RETURN(windowHandle == NULL);
+
+
+    ShowWindow(windowHandle, SW_SHOW);
+    //Message Loop
+    MSG windowMsg= {};
+    while (windowMsg.message != WM_QUIT) {
+        GetMessage(&windowMsg, NULL, 0, 0);
+        DispatchMessage(&windowMsg);
+    }
+
+
 
 
 
